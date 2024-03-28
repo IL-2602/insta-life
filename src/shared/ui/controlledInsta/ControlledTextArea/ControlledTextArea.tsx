25ac63d69b33cc86d6f6dfc2fe0430bee1fa7237
Brainman17 <113635410+Brainman17@users.noreply.github.com>
2024-03-29 00:26:00 +0300

    IL-59: add controlled text area

---
+++ src/shared/ui/controlledInsta/ControlledTextArea/ControlledTextArea.tsx
@@ -0,0 +1,23 @@
+import { Ref } from 'react'
+import { Control, FieldPath, FieldValues, useController } from 'react-hook-form'
+
+import { TextArea, TextAreaFieldProps } from '@/shared/ui/TextArea'
+
+type Props<T extends FieldValues> = {
+  control: Control<T>
+  name: FieldPath<T>
+} & Omit<TextAreaFieldProps, 'onChange' | 'value'>
+export const ControlledTextAreaField = <T extends FieldValues>(
+  { control, name, ...rest }: Props<T>,
+  ref: Ref<HTMLInputElement>
+) => {
+  const {
+    field,
+    fieldState: { error },
+  } = useController({
+    control,
+    name,
+  })
+
+  return <TextArea {...field} errorMessage={error?.message} {...rest} />
+}
