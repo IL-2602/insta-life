52541c06af1e05dc3ee7d9ba75ea1d6cfc0fed8a
ZanziPanziBY <zanzipanziby@gmail.com>
2024-03-29 12:59:35 +0300

    IL-63 fix: delete profileSlice

--- src/widgets/profile/profileHeader/ui/ProfileHeader.tsx
+++ src/widgets/profile/profileHeader/ui/ProfileHeader.tsx
@@ -1,14 +1,26 @@
+import { Spinner } from '@/shared/ui/Spinner'
 import { ProfileHeaderProps } from '@/widgets/profile/profileHeader/container'
 import { Avatar } from '@/widgets/profile/profileHeader/ui/Avatar'
 import { ProfileInfo } from '@/widgets/profile/profileHeader/ui/ProfileInfo/ProfileInfo'
 
 import s from './ProfileHeader.module.scss'
 
-export const ProfileHeader = ({ aboutMe, avatars, userName }: ProfileHeaderProps) => {
-  return (
-    <div className={s.wrapper}>
-      <Avatar avatar={avatars[0]} />
-      <ProfileInfo aboutMe={aboutMe} userName={userName} />
-    </div>
-  )
+export const ProfileHeader = (props: ProfileHeaderProps) => {
+  const { data, isError, isLoading } = props
+
+  if (isLoading) {
+    return <Spinner />
+  }
+  if (data) {
+    const { aboutMe, avatars, userName } = data
+
+    return (
+      <div className={s.wrapper}>
+        <Avatar avatar={avatars[0]} />
+        <ProfileInfo aboutMe={aboutMe} userName={userName} />
+      </div>
+    )
+  }
+
+  return null
 }
