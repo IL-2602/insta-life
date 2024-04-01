80742b4dfed5e23aae526175fbef8ebbbc0b5ba2
Brainman17 <113635410+Brainman17@users.noreply.github.com>
2024-04-01 16:51:03 +0300

    IL-57: implement Controller from react hook form

--- src/shared/ui/Calendar/Calendar.tsx
+++ src/shared/ui/Calendar/Calendar.tsx
@@ -1,54 +1,70 @@
-import React, { useState } from 'react'
+import React from 'react'
 import DatePicker from 'react-datepicker'
+import { Controller } from 'react-hook-form'
 
-import { useTranslation } from '@/shared/hooks/useTranslation'
 import { CustomHeader } from '@/shared/ui/Calendar/CustomHeader'
-import { ControlledTextField } from '@/shared/ui/controlledInsta/ControlledTextField/ControlledTextField'
+import { TextField } from '@/shared/ui/Textfield'
 import { enUS, ru } from 'date-fns/locale'
 import { useRouter } from 'next/router'
 
-import './Calendar.scss'
 import 'react-datepicker/dist/react-datepicker.css'
+
+import moduleStyles from './Calendar.module.scss'
+
+import './Calendar.scss' // eslint-disable-line
 
 import CalendarIcon from '../../assets/icons/Calendar/Calendar'
 
 type Props = {
   control: any
-  errorDateOfBirth: string | undefined
-  errorMessage: string | undefined
+  errorMessage?: string | undefined
+  name: string
 }
-export const Calendar = ({ control, errorDateOfBirth, errorMessage }: Props) => {
-  const [startDate, setStartDate] = useState(new Date())
-  const { t } = useTranslation()
-
+export const Calendar = ({ control, errorMessage, name }: Props) => {
   const { locale } = useRouter()
 
   return (
-    <DatePicker
-      customInput={
-        <ControlledTextField
-          control={control}
-          errorMessage={errorDateOfBirth}
-          iconEnd={<CalendarIcon />}
-          name={'dateOfBirth'}
-        />
-      }
-      dateFormatCalendar={'YYYY'}
-      formatWeekDay={date => date.substring(0, 3)}
-      locale={locale === 'ru' ? ru : enUS}
-      onChange={(date: any) => setStartDate(date)}
-      renderCustomHeader={({ changeMonth, changeYear, date, decreaseMonth, increaseMonth }) => (
-        <CustomHeader
-          changeMonth={changeMonth}
-          changeYear={changeYear}
-          date={date}
-          decreaseMonth={decreaseMonth}
-          increaseMonth={increaseMonth}
-        />
-      )}
-      selected={startDate}
-      showMonthDropdown
-      showYearDropdown
-    />
+    <>
+      <Controller
+        control={control}
+        name={name}
+        render={({ field: { onBlur, onChange, ref, value } }) => {
+          return (
+            <DatePicker
+              className={`${moduleStyles}`}
+              customInput={
+                <TextField
+                  defaultValue={'00/00/0000'}
+                  errorMessage={errorMessage}
+                  iconEnd={<CalendarIcon />}
+                />
+              }
+              dateFormat={'dd/MM/yyyy'}
+              formatWeekDay={date => date.substring(0, 3)}
+              locale={locale === 'ru' ? ru : enUS}
+              onChange={onChange}
+              renderCustomHeader={({
+                changeMonth,
+                changeYear,
+                date,
+                decreaseMonth,
+                increaseMonth,
+              }) => (
+                <CustomHeader
+                  changeMonth={changeMonth}
+                  changeYear={changeYear}
+                  date={date}
+                  decreaseMonth={decreaseMonth}
+                  increaseMonth={increaseMonth}
+                />
+              )}
+              selected={value}
+              showMonthDropdown
+              showYearDropdown
+            />
+          )
+        }}
+      />
+    </>
   )
 }
