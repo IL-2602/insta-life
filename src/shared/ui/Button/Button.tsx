538116ff27c5fde4f056ac961498a26204720896
Papperla <krivman@Savar.local>
2024-04-05 16:41:23 +0200

    IL-67 feat: add forward ref in Button component

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,52 +1,56 @@
-import React, { ComponentPropsWithoutRef, ElementType, ReactNode } from 'react'
+import React, { ElementRef, ElementType, ReactNode, forwardRef } from 'react'
 
+import { PolymorphComponentPropsWithRef } from '@/shared/types/polymorfic/polymorfic'
 import Link from 'next/link'
 
 import s from './Button.module.scss'
 
 import { Spinner } from '../Spinner/Spinner'
 
-export type ButtonProps<T extends ElementType = 'button'> = {
-  as?: T
-  children: ReactNode
+export type CustomProps<T extends ElementType = 'button'> = {
   className?: string
   fullWidth?: boolean
   isLoading?: boolean
   variant?: 'link' | 'outlined' | 'primary' | 'secondary'
-} & ComponentPropsWithoutRef<T>
+}
 
-// С помощью Omit мы убираем из пропсов переданного компонента все пропсы,
-// которые уже есть в наших кастомных пропсах, тем самым избегая коллизий.
-export const Button = <T extends ElementType = 'button'>(
-  props: ButtonProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof ButtonProps<T>>
-) => {
-  const {
-    as: Component = 'button',
-    children,
-    className,
-    fullWidth,
-    isLoading,
-    variant = 'primary',
-    ...rest
-  } = props
+type Props<T extends ElementType> = PolymorphComponentPropsWithRef<T, CustomProps>
 
-  if (Component === 'a') {
+type ButtonComponent = <T extends ElementType = 'button'>(props: Props<T>) => ReactNode
+export const Button: ButtonComponent = forwardRef(
+  <T extends ElementType = 'button'>(props: Props<T>, ref: ElementRef<T>) => {
+    const {
+      asComponent: Component = 'button',
+      children,
+      className,
+      fullWidth,
+      isLoading,
+      variant = 'primary',
+      ...rest
+    } = props
+
+    if (Component === 'a') {
+      return (
+        <Link
+          className={`${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className}`}
+          href={rest.href}
+          {...rest}
+        >
+          {children}
+          {isLoading && <Spinner />}
+        </Link>
+      )
+    }
+
     return (
-      <Link
+      <Component
         className={`${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className}`}
-        href={rest.href}
         {...rest}
+        ref={ref}
       >
         {children}
         {isLoading && <Spinner />}
-      </Link>
+      </Component>
     )
   }
-
-  return (
-    <Component className={`${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className}`} {...rest}>
-      {children}
-      {isLoading && <Spinner />}
-    </Component>
-  )
-}
+)
