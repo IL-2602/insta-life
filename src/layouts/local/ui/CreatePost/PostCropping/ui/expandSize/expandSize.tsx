3ca01b879304d8ed6fc48403ce7683e8800c7ad5
Mannomann <krivmann00@gmail.com>
2024-04-07 23:43:47 +0200

    IL-67 feat: refactor components

--- src/layouts/local/ui/CreatePost/PostCropping/ui/expandSize/expandSize.tsx
+++ src/layouts/local/ui/CreatePost/PostCropping/ui/expandSize/expandSize.tsx
@@ -1,18 +1,21 @@
 import { Button } from '@/shared/ui/Button'
 import { DropMenu } from '@/shared/ui/DropMenu'
+import { Typography } from '@/shared/ui/Typography'
 import Image from 'next/image'
+
 import s from './expandSize.module.scss'
+
 import expand from '../../../../../../../../public/assets/expand-outline.svg'
-import original from '../../../../../../../../public/assets/postCropping/original.svg'
 import one from '../../../../../../../../public/assets/postCropping/1x1.svg'
 import four from '../../../../../../../../public/assets/postCropping/4x5.svg'
 import sixteen from '../../../../../../../../public/assets/postCropping/16x9.svg'
-import { Typography } from '@/shared/ui/Typography'
+import original from '../../../../../../../../public/assets/postCropping/original.svg'
 
 export const ExpandSize = () => {
   return (
     <DropMenu.Menu
       align={'start'}
+      className={s.container}
       side={'top'}
       sideOffset={2}
       trigger={
@@ -20,23 +23,22 @@ export const ExpandSize = () => {
           <Image alt={'Image expand'} height={24} src={expand} width={24} />
         </Button>
       }
-      className={s.container}
     >
       <DropMenu.Item>
         <Typography variant={'h3'}>Original</Typography>
-        <Image src={original} alt={'Original size'} height={24} width={24} />
+        <Image alt={'Original size'} height={24} src={original} width={24} />
       </DropMenu.Item>
       <DropMenu.Item>
         <Typography variant={'regular14'}>1:1</Typography>
-        <Image src={one} alt={'1:1 size'} height={18} width={18} />
+        <Image alt={'1:1 size'} height={18} src={one} width={18} />
       </DropMenu.Item>
       <DropMenu.Item>
         <Typography variant={'regular14'}>4:5</Typography>
-        <Image src={four} alt={'4:5 size'} height={26} width={18} />
+        <Image alt={'4:5 size'} height={26} src={four} width={18} />
       </DropMenu.Item>
       <DropMenu.Item>
         <Typography variant={'regular14'}>16:9</Typography>
-        <Image src={sixteen} alt={'16:9 size'} height={18} width={26} />
+        <Image alt={'16:9 size'} height={18} src={sixteen} width={26} />
       </DropMenu.Item>
     </DropMenu.Menu>
   )
