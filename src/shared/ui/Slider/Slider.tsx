82dc1d5fdd866cc67cb3922581a681334d8f4af6
Papperla <krivman@Savar.local>
2024-04-08 11:44:11 +0200

    IL-67 feat: add slider from radix

---
+++ src/shared/ui/Slider/Slider.tsx
@@ -0,0 +1,43 @@
+import * as RadixSlider from '@radix-ui/react-slider'
+
+import s from './Slider.module.scss'
+
+export const Slider = ({
+  label,
+  max,
+  min = 0,
+  minStepsBetweenThumbs = 1,
+  onValueChange,
+  slidersValue,
+  step = 1,
+}: PropsType) => {
+  return (
+    <div className={s.container}>
+      <div className={s.wrapper}>
+        <RadixSlider.Root
+          className={s.root}
+          max={max}
+          min={min}
+          minStepsBetweenThumbs={minStepsBetweenThumbs}
+          onValueChange={onValueChange}
+          step={step}
+          value={slidersValue}
+        >
+          <RadixSlider.Track className={s.track}>
+            <RadixSlider.Range className={s.range} />
+          </RadixSlider.Track>
+          <RadixSlider.Thumb aria-label={'Volume'} className={s.thumb} />
+        </RadixSlider.Root>
+      </div>
+    </div>
+  )
+}
+type PropsType = {
+  label?: string
+  max?: number
+  min?: number
+  minStepsBetweenThumbs?: number
+  onValueChange: (slidersValue: [number]) => void
+  slidersValue: [number]
+  step?: number
+}
