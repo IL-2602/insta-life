bd6bf75c3022114b9adb5c68160572dc8787dc30
ZanziPanziBY <zanzipanziby@gmail.com>
2024-04-08 21:17:44 +0300

    IL-68 feat: implement basic photo-filter modal

--- src/widgets/create/PhotoFilter/container/useContainer.ts
+++ src/widgets/create/PhotoFilter/container/useContainer.ts
@@ -7,43 +7,46 @@ export const useContainer = () => {
   const canvasRef = useRef<HTMLCanvasElement>(null)
   const [currentImage, setCurrentImage] = useState<Photo>(mockPhotoData[0])
   const [currentFilter, setCurrentFilter] = useState<PhotoFilterTitle>('normal')
+  const [loadedImage, setLoadedImage] = useState<HTMLImageElement | null>(null) // Состояние для хранения загруженного изображения
   const [savedImage, setSavedImage] = useState<string>('')
   const [modalIsOpen, setModalIsOpen] = useState(false)
 
+  // Загрузка изображения
   useEffect(() => {
-    if (canvasRef.current && currentImage.url && modalIsOpen) {
+    const image = new Image()
+
+    image.crossOrigin = 'anonymous' // Для CORS
+    image.onload = () => setLoadedImage(image) // Устанавливаем загруженное изображение в состояние
+    image.src = currentImage.url
+  }, [currentImage])
+
+  // Отрисовка на канвасе после загрузки изображения и при изменении фильтра/открытия модалки
+  useEffect(() => {
+    if (canvasRef.current && loadedImage && modalIsOpen) {
       const ctx = canvasRef.current.getContext('2d')!
-      const image = new Image()
+      const fixedWidth = 500
+      const aspectRatio = loadedImage.height / loadedImage.width
+      const fixedHeight = fixedWidth * aspectRatio
 
-      image.crossOrigin = 'anonymous' // Необходимо для избежания CORS ограничений
-      image.onload = () => {
-        const fixedWidth = 500
-        const aspectRatio = image.height / image.width
-        const fixedHeight = fixedWidth * aspectRatio
-
-        canvasRef.current!.width = fixedWidth
-        canvasRef.current!.height = fixedHeight
-        ctx.clearRect(0, 0, canvasRef.current!.width, canvasRef.current!.height)
-        ctx.filter = currentFilter
-        ctx.drawImage(image, 0, 0, fixedWidth, fixedHeight)
-      }
-      image.src = currentImage.url
+      canvasRef.current.width = fixedWidth
+      canvasRef.current.height = fixedHeight
+      ctx.clearRect(0, 0, fixedWidth, fixedHeight)
+      ctx.filter = currentFilter || 'none'
+      ctx.drawImage(loadedImage, 0, 0, fixedWidth, fixedHeight)
     }
-  }, [currentImage, currentFilter, modalIsOpen])
+  }, [loadedImage, currentFilter, modalIsOpen])
 
   const applyFilter = (filter: PhotoFilterTitle) => {
     setCurrentFilter(filter)
   }
 
   const saveImage = () => {
-    if (!canvasRef.current) {
-      return
-    }
-    const editedImageUrl = canvasRef.current.toDataURL('image/png')
+    if (canvasRef.current) {
+      const editedImageUrl = canvasRef.current.toDataURL('image/png')
 
-    // Здесь можно выполнить дальнейшие действия с editedImageUrl, например, отправить на сервер или сохранить локально
-    setSavedImage(editedImageUrl)
-    console.log('Сохраненное изображение URL:', editedImageUrl)
+      setSavedImage(editedImageUrl)
+      console.log('Сохраненное изображение URL:', editedImageUrl)
+    }
   }
 
   return {
--- src/widgets/create/PhotoFilter/ui/PhotoFilter.tsx
+++ src/widgets/create/PhotoFilter/ui/PhotoFilter.tsx
@@ -3,46 +3,117 @@ import React, { forwardRef, useEffect, useRef, useState } from 'react'
 import { Button } from '@/shared/ui/Button'
 import { Modal } from '@/shared/ui/Modal'
 import { FilterPreviewButton } from '@/widgets/create/PhotoFilter/FilterPreviewButton/FilterPreviewButton'
-import { photoFilters } from '@/widgets/create/PhotoFilter/FilterPreviewButton/FilterPreviewButtonData'
+import {
+  PhotoFilterTitle,
+  photoFilters,
+} from '@/widgets/create/PhotoFilter/FilterPreviewButton/FilterPreviewButtonData'
 import { PhotoFilterProps } from '@/widgets/create/PhotoFilter/container'
+import { Photo, mockPhotoData } from '@/widgets/create/PhotoFilter/mockPhotoData'
 import { capitalizeFirstLetter } from '@/widgets/create/PhotoFilter/utils/capitalizeFirstLetter'
 
-export const PhotoFilter = forwardRef<HTMLCanvasElement, PhotoFilterProps>(
-  ({ applyFilter, currentImage, modalIsOpen, saveImage, savedImage, setModalIsOpen }, ref) => {
-    const openModalHandle = () => {
-      setModalIsOpen(true)
-      applyFilter('normal')
+export const PhotoFilter = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null)
+  const [currentImage, setCurrentImage] = useState<Photo>(mockPhotoData[0])
+  const [currentFilter, setCurrentFilter] = useState<PhotoFilterTitle>('normal')
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+
+  useEffect(() => {
+    const image = new Image()
+
+    image.crossOrigin = 'anonymous' // Для CORS
+    image.onload = () => {
+      if (canvasRef.current && modalIsOpen) {
+        const canvas = canvasRef.current
+        const ctx = canvas.getContext('2d')
+
+        if (!ctx) {
+          console.error('Не удалось получить контекст канваса')
+
+          return
+        }
+
+        // Предполагаем, что родительский элемент канваса имеет заданные CSS-размеры.
+        const canvasWidth = canvas.offsetWidth // Ширина родителя
+        const canvasHeight = canvas.offsetHeight // Высота родителя
+
+        // Применяем размеры канваса
+        canvas.width = canvasWidth
+        canvas.height = canvasHeight
+
+        // Рассчитываем новые пропорции для масштабирования изображения
+        const imgAspectRatio = image.width / image.height
+        const canvasAspectRatio = canvasWidth / canvasHeight
+        let drawWidth, drawHeight
+
+        if (imgAspectRatio > canvasAspectRatio) {
+          // Изображение шире канваса
+          drawHeight = canvasHeight
+          drawWidth = canvasHeight * imgAspectRatio
+        } else {
+          // Изображение выше канваса
+          drawWidth = canvasWidth
+          drawHeight = canvasWidth / imgAspectRatio
+        }
+
+        // Рассчитываем смещение для центрирования изображения
+        const xOffset = (canvasWidth - drawWidth) / 2
+        const yOffset = (canvasHeight - drawHeight) / 2
+
+        // Очищаем канвас
+        ctx.clearRect(0, 0, canvasWidth, canvasHeight)
+        ctx.filter = currentFilter || 'none'
+
+        // Отрисовываем изображение с учетом смещения
+        ctx.drawImage(image, xOffset, yOffset, drawWidth, drawHeight)
+      }
     }
+    image.src = currentImage.url
+  }, [currentImage, currentFilter, modalIsOpen])
 
-    return (
-      <>
-        <Button onClick={openModalHandle}>Open</Button>
-        <Modal onSubmit={() => setModalIsOpen(false)} open={modalIsOpen}>
-          <div
-            style={{ alignItems: 'flex-start', display: 'flex', justifyContent: 'space-between' }}
-          >
-            <canvas ref={ref} />
-            <div>
-              {photoFilters.map(filter => (
-                <FilterPreviewButton
-                  applyFilter={applyFilter}
-                  filter={filter}
-                  imageUrl={currentImage.url}
-                  key={filter}
-                  label={capitalizeFirstLetter(filter)}
-                />
-              ))}
-            </div>
-            <button onClick={saveImage} style={{ border: '2px solid var(--color-primary-100)' }}>
-              Сохранить
-            </button>
-          </div>
-        </Modal>
-
-        {savedImage && (
-          <img alt={'Saved'} src={savedImage} style={{ marginTop: '20px' }} width={'200px'} />
-        )}
-      </>
-    )
+  const applyFilter = (filter: PhotoFilterTitle) => {
+    setCurrentFilter(filter)
   }
-)
+
+  const saveImage = () => {
+    if (canvasRef.current) {
+      const editedImageUrl = canvasRef.current.toDataURL('image/png')
+
+      console.log('Сохраненное изображение URL:', editedImageUrl)
+    }
+  }
+
+  const openModalHandle = () => {
+    setModalIsOpen(true)
+    applyFilter('normal') // Это может быть необходимо переместить в useEffect, если 'normal' вызывает перерисовку
+  }
+
+  return (
+    <>
+      <Button onClick={openModalHandle}>Open</Button>
+
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          width: '1200px',
+        }}
+      >
+        <div style={{ width: '50%' }}>
+          <canvas ref={canvasRef} />
+        </div>
+        <div>
+          {photoFilters.map(filter => (
+            <FilterPreviewButton
+              applyFilter={applyFilter}
+              filter={filter}
+              imageUrl={currentImage.url}
+              key={filter}
+              label={capitalizeFirstLetter(filter)}
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  )
+}
