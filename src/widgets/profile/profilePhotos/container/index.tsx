26897ab379e863ba4b3e278fe15276ab9f1535e8
Svyatoslav Nemkovich <svyat5555@gmail.com>
2024-04-12 22:13:23 +0300

    fix: remove unnecessary types

--- src/widgets/profile/profilePhotos/container/index.tsx
+++ src/widgets/profile/profilePhotos/container/index.tsx
@@ -3,6 +3,6 @@ import { FC } from 'react'
 import { ProfilePhotos } from '../ui/ProfilePhotos'
 import { useContainer } from './useContainer'
 
-export const Container: FC<ProfilePhotosProps> = () => <ProfilePhotos {...useContainer()} />
+export const Container = () => <ProfilePhotos {...useContainer()} />
 
 export type ProfilePhotosProps = ReturnType<typeof useContainer>
