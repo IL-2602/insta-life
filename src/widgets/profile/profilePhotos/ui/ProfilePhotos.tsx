4434697258096a5021c9216cb179566305cff18f
Brainman17 <113635410+Brainman17@users.noreply.github.com>
2024-04-19 14:27:24 +0300

    IL-71: test

--- src/widgets/profile/profilePhotos/ui/ProfilePhotos.tsx
+++ src/widgets/profile/profilePhotos/ui/ProfilePhotos.tsx
@@ -7,21 +7,16 @@ import Image from 'next/image'
 import s from './ProfilePhotos.module.scss'
 
 export const ProfilePhotos = forwardRef<HTMLDivElement, ProfilePhotosProps>(
-  ({ isFetching, photos, test }, ref) => {
+  ({ isFetching, photos }, ref) => {
     return (
       <>
         <div className={s.container}>
-          {test.length === photos.length
-            ? test.map((photo: string, i: number) => (
-                <div className={s.imageContainer} key={i}>
-                  <Image alt={'postPhotos'} height={228} src={photo} width={234} />
-                </div>
-              ))
-            : photos.map((photo: string, i: number) => (
-                <div className={s.imageContainer} key={i}>
-                  <Image alt={'postPhotos'} height={228} src={photo} width={234} />
-                </div>
-              ))}
+          {photos &&
+            photos.map((photo: string, i: number) => (
+              <div className={s.imageContainer} key={i}>
+                <Image alt={'postPhotos'} height={228} src={photo} width={234} />
+              </div>
+            ))}
           <div ref={ref} />
         </div>
         {isFetching && (
