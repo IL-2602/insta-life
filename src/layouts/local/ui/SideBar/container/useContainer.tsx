2c080d7ff5d0ea80738e6afce6aa0c8928cad2c2
Papperla <krivman@Savar.local>
2024-05-03 10:55:04 +0200

    IL-101 feat: rework LogOut Modal, add remove api state and add redirect to /auth/sign-in

--- src/layouts/local/ui/SideBar/container/useContainer.tsx
+++ src/layouts/local/ui/SideBar/container/useContainer.tsx
@@ -7,24 +7,26 @@ import { UserType } from '@/services/authService/lib/authEndpoints.types'
 import { postActions } from '@/services/postService/store/slice/postEndpoints.slice'
 import { useTranslation } from '@/shared/hooks/useTranslation'
 import { useRouter } from 'next/router'
+import { ROUTES } from '@/shared/constants/routes'
 
 export const useContainer = () => {
   const { t } = useTranslation()
   const dispatch = useAppDispatch()
   const router = useRouter()
-  const { pathname } = router
+  const { pathname, isReady } = router
 
   const [isOpen, setIsOpen] = useState(false)
 
   const email = useAppSelector(state => state.authReducer.email)
   const isCreatePostModal = useAppSelector(state => state.postReducer.isCreatePostModal)
 
-  const [logOut, { isLoading }] = useLogOutMutation()
+  const [logOut, { isLoading: isLoadingLogOut }] = useLogOutMutation()
   const { data: me } = useGetMeQuery() as { data: UserType }
   const handleLogOut = () => {
-    router.push('/')
+    setIsOpen(false)
     logOut()
       .unwrap()
+      .then(() => router.push(ROUTES.LOGIN))
       .catch(e => console.log(e))
   }
 
@@ -37,6 +39,7 @@ export const useContainer = () => {
     return pathname === path && !isCreatePostModal
   }
 
+  const isLoading = isLoadingLogOut || !isReady
   return {
     email,
     handleActiveLink,
