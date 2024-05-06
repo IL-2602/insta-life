d033038d24170a5d11a61713d547afb63f26eb0b
Papperla <krivman@Savar.local>
2024-05-06 17:07:01 +0200

    IL-72 feat: rework modal style, add overlay, fix modal size

--- src/layouts/local/ui/MyPost/MyPostModal/ui/MyPostModal.tsx
+++ src/layouts/local/ui/MyPost/MyPostModal/ui/MyPostModal.tsx
@@ -21,6 +21,7 @@ import Image from 'next/image'
 import s from './MyPostModal.module.scss'
 
 import noPhoto from '../../../../../../../public/assets/noPhoto.svg'
+import { createPortal } from 'react-dom'
 
 type PhotoType = {
   createdAt: string
@@ -57,6 +58,26 @@ export const MyPostModal = memo(
     if (!postId) {
       return null
     }
+    if (isLoading) {
+      return (
+        <div
+          style={{
+            background: 'rgba(0, 0, 0, 0.5)',
+            height: '100%',
+            position: 'fixed',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+            alignItems: 'center',
+            display: 'flex',
+            justifyContent: 'center',
+            width: '100%',
+          }}
+        >
+          <Spinner />
+        </div>
+      )
+    }
 
     return (
       <>
@@ -67,105 +88,86 @@ export const MyPostModal = memo(
           open={isMyPostModal}
         >
           <div className={s.container}>
-            {isLoading ? (
-              <div
-                style={{
-                  alignItems: 'center',
-                  display: 'flex',
-                  height: '564px',
-                  justifyContent: 'center',
-                  width: '100%',
-                }}
-              >
-                <Spinner />
-              </div>
-            ) : (
-              <>
-                <div className={s.postPhotoWrapper}>
-                  <PostPhotos
-                    currentPhoto={currPhotoIndex}
-                    onChangeCurrentPhoto={onChangeCurrPhoto}
-                  >
-                    {postPhotos &&
-                      postPhotos.images.map((photo: PhotoType, i: number) => {
-                        return (
-                          <div className={s.postPhotoWrapper} key={i}>
-                            <Image alt={'photo'} className={s.postPhoto} fill src={photo.url} />
-                          </div>
-                        )
-                      })}
-                  </PostPhotos>
-                </div>
-                <div className={s.descriptionWrapper}>
-                  <div className={s.userWrapper}>
-                    <div className={s.userContainer}>
-                      <div className={s.userPhotoWrapper}>
-                        <div className={s.photo}>
-                          {getProfile?.avatars[0] === undefined ? (
-                            <Image alt={'noUserPhoto'} height={22} src={noPhoto} width={22} />
-                          ) : (
-                            <Image
-                              alt={'userPhoto'}
-                              height={36}
-                              src={postPhotos?.avatarOwner}
-                              width={36}
-                            />
-                          )}
-                        </div>
-                        <Typography variant={'h3'}>{postPhotos?.userName}</Typography>
+            <div className={s.postPhotoWrapper}>
+              <PostPhotos currentPhoto={currPhotoIndex} onChangeCurrentPhoto={onChangeCurrPhoto}>
+                {postPhotos &&
+                  postPhotos.images.map((photo: PhotoType, i: number) => {
+                    return (
+                      <div className={s.postPhotoWrapper} key={i}>
+                        <Image alt={'photo'} className={s.postPhoto} fill src={photo.url} />
                       </div>
+                    )
+                  })}
+              </PostPhotos>
+            </div>
+            <div className={s.descriptionWrapper}>
+              <div className={s.userWrapper}>
+                <div className={s.userContainer}>
+                  <div className={s.userPhotoWrapper}>
+                    <div className={s.photo}>
+                      {getProfile?.avatars[0] === undefined ? (
+                        <Image alt={'noUserPhoto'} height={22} src={noPhoto} width={22} />
+                      ) : (
+                        <Image
+                          alt={'userPhoto'}
+                          height={36}
+                          src={postPhotos?.avatarOwner}
+                          width={36}
+                        />
+                      )}
                     </div>
+                    <Typography variant={'h3'}>{postPhotos?.userName}</Typography>
+                  </div>
+                </div>
 
-                    <div className={s.postOptions}>
-                      <CustomPopover
-                        contentChildren={
-                          <PostOptions
-                            deletePostModalHandler={deletePostModalHandler}
-                            // editModeHandler={editModeHandler}
-                            editPostModalHandler={editPostModalHandler}
-                            id={'123'}
-                          />
-                        }
-                        icon={
-                          <div style={{ position: 'relative' }}>
-                            <HorizontalDots />
-                          </div>
-                        }
+                <div className={s.postOptions}>
+                  <CustomPopover
+                    contentChildren={
+                      <PostOptions
+                        deletePostModalHandler={deletePostModalHandler}
+                        // editModeHandler={editModeHandler}
+                        editPostModalHandler={editPostModalHandler}
+                        id={'123'}
                       />
-                    </div>
-                  </div>
-
-                  <div className={s.commentsBlock}>
-                    <ScrollSelect maxHeight={'300px'} type={'always'}>
-                      <TestComment />
-                      <TestComment />
-                      <TestComment />
-                    </ScrollSelect>
-                  </div>
-                  <div className={s.likesBlock}>
-                    <div className={s.buttonIcons}>
-                      <div>
-                        <HeartOutline className={s.buttonIcon} />
-                        <PaperLine className={s.buttonIcon} />
+                    }
+                    icon={
+                      <div style={{ position: 'relative' }}>
+                        <HorizontalDots />
                       </div>
-
-                      <Bookmark className={s.buttonIcon} />
-                    </div>
-                  </div>
-                  <div className={s.addCommentBlock}>
-                    <input placeholder={'Add comment...'} type={'text'} />
-                    <Button
-                      className={s.button}
-                      disabled={false}
-                      onClick={commentPublish}
-                      variant={'outlined'}
-                    >
-                      <Typography variant={'h3'}>{t.button.publish}</Typography>
-                    </Button>
-                  </div>
+                    }
+                  />
                 </div>
-              </>
-            )}
+              </div>
+
+              <div className={s.commentsBlock}>
+                <ScrollSelect maxHeight={'300px'} type={'always'}>
+                  <TestComment />
+                  <TestComment />
+                  <TestComment />
+                </ScrollSelect>
+              </div>
+              <div className={s.likesBlock}>
+                <div className={s.buttonIcons}>
+                  <div>
+                    <HeartOutline className={s.buttonIcon} />
+                    <PaperLine className={s.buttonIcon} />
+                  </div>
+
+                  <Bookmark className={s.buttonIcon} />
+                </div>
+              </div>
+              <div className={s.addCommentBlock}>
+                <input placeholder={'Add comment...'} type={'text'} />
+                <Button
+                  className={s.button}
+                  disabled={false}
+                  onClick={commentPublish}
+                  variant={'outlined'}
+                >
+                  <Typography variant={'h3'}>{t.button.publish}</Typography>
+                </Button>
+              </div>
+            </div>
           </div>
         </Modal>
         <EditPostModal.widget />
