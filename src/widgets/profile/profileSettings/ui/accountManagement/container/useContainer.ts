833381df1db319b68b99826c368dbd65d0065f5d
Brainman17 <113635410+Brainman17@users.noreply.github.com>
2024-05-11 21:46:48 +0300

    IL-115: add accountManagement jsx and container

--- src/widgets/profile/profileSettings/ui/accountManagement/container/useContainer.ts
+++ src/widgets/profile/profileSettings/ui/accountManagement/container/useContainer.ts
@@ -1,3 +1,115 @@
+import { ChangeEvent, useState } from 'react'
+
+import { usePostSubscriptionsMutation } from '@/services/subscriptionsService/subscriptionsEndpoints'
+import { FRONTEND_URL } from '@/shared/constants/frontendUrl'
+import { useTranslation } from '@/shared/hooks/useTranslation'
+import { useRouter } from 'next/router'
+
 export const useContainer = () => {
-  return {}
+  const [accountType, setAccountType] = useState('personal')
+  const [subscriptionCost, setSubscriptionCost] = useState('dailySubscription')
+  const [isModalSubscription, setIsModalSubscription] = useState(false)
+
+  const { t } = useTranslation()
+  const router = useRouter()
+
+  const accountTypes = [
+    {
+      id: 'personal-account',
+      name: 'personal-account',
+      title: t.profileSettings.tab.accountManagement.accountTypePersonal,
+      value: 'personal',
+    },
+    {
+      id: 'business-account',
+      name: 'business-account',
+      title: t.profileSettings.tab.accountManagement.accountTypeBusiness,
+      value: 'business',
+    },
+  ]
+
+  const subscriptionCosts = [
+    {
+      id: 'dailySubscription',
+      name: 'dailySubscription',
+      title: t.profileSettings.tab.accountManagement.accountTypeBusinessPrice.perDay,
+      value: 'dailySubscription',
+    },
+    {
+      id: 'weeklySubscription',
+      name: 'weeklySubscription',
+      title: t.profileSettings.tab.accountManagement.accountTypeBusinessPrice.perWeek,
+      value: 'weeklySubscription',
+    },
+    {
+      id: 'monthlySubscription',
+      name: 'monthlySubscription',
+      title: t.profileSettings.tab.accountManagement.accountTypeBusinessPrice.perMonth,
+      value: 'monthlySubscription',
+    },
+  ]
+
+  const accountTypeChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setAccountType(event.target.value)
+  }
+
+  const subscriptionCostChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSubscriptionCost(event.target.value)
+  }
+
+  type SubscriptionsType = {
+    [key: string]: { amount: number; type: 'DAY' | 'MONTHLY' | 'WEEKLY' }
+  }
+
+  const subscriptions: SubscriptionsType = {
+    dailySubscription: {
+      amount: 10,
+      type: 'DAY',
+    },
+    monthlySubscription: {
+      amount: 100,
+      type: 'MONTHLY',
+    },
+    weeklySubscription: {
+      amount: 50,
+      type: 'WEEKLY',
+    },
+  }
+
+  const [postSubscriptions, { isLoading, isSuccess }] = usePostSubscriptionsMutation()
+
+  const handlePayment = async (typePayment: 'PAYPAL' | 'STRIPE') => {
+    const body = {
+      amount: subscriptions[subscriptionCost].amount,
+      baseUrl: FRONTEND_URL,
+      paymentType: typePayment,
+      typeSubscription: subscriptions[subscriptionCost].type,
+    } as const
+
+    try {
+      const { url } = await postSubscriptions(body).unwrap()
+
+      // window.open(url, '_blank')
+      await router.push(url)
+    } catch (err) {
+      console.log(err)
+    } finally {
+      setIsModalSubscription(true)
+    }
+  }
+
+  return {
+    accountType,
+    accountTypeChange,
+    accountTypes,
+    handlePayment,
+    isLoading,
+    isModalSubscription,
+    isSuccess,
+    setIsModalSubscription,
+    subscriptionCost,
+    subscriptionCostChange,
+    subscriptionCosts,
+    t,
+  }
 }
--- src/widgets/profile/profileSettings/ui/accountManagement/ui/AccountManagement.tsx
+++ src/widgets/profile/profileSettings/ui/accountManagement/ui/AccountManagement.tsx
@@ -1,9 +1,124 @@
 import { memo } from 'react'
 
+import { PayPal } from '@/shared/assets/icons/PayPal'
+import { Stripe } from '@/shared/assets/icons/Stripe'
+import { Button } from '@/shared/ui/Button'
+import { Modal } from '@/shared/ui/Modal'
+import { Spinner } from '@/shared/ui/Spinner'
+import { Typography } from '@/shared/ui/Typography'
 import { AccountManagementProps } from '@/widgets/profile/profileSettings/ui/accountManagement/container'
+import { clsx } from 'clsx'
 
 import s from './AccountManagement.module.scss'
 
-export const AccountManagement = memo(({}: AccountManagementProps) => {
-  return <div className={s.container}>Devices</div>
-})
+export const AccountManagement = memo(
+  ({
+    accountType,
+    accountTypeChange,
+    accountTypes,
+    handlePayment,
+    isLoading,
+    isModalSubscription,
+    isSuccess,
+    setIsModalSubscription,
+    subscriptionCost,
+    subscriptionCostChange,
+    subscriptionCosts,
+    t,
+  }: AccountManagementProps) => {
+    return (
+      <>
+        {isLoading && (
+          <div className={s.spinner}>
+            <Spinner />
+          </div>
+        )}
+        <div className={clsx(s.container, isLoading ? s.opacity : '')}>
+          <fieldset className={s.accountTypes}>
+            <Typography as={'legend'} variant={'h3'}>
+              {t.profileSettings.tab.accountManagement.accountType}
+            </Typography>
+            <div className={s.radioWrapper}>
+              {accountTypes.map((account, index) => (
+                <div key={index}>
+                  <input
+                    checked={accountType === account.value}
+                    id={account.id}
+                    name={account.name}
+                    onChange={accountTypeChange}
+                    type={'radio'}
+                    value={account.value}
+                  />
+                  <Typography as={'label'} htmlFor={account.id} variant={'regular14'}>
+                    {account.title}
+                  </Typography>
+                </div>
+              ))}
+            </div>
+          </fieldset>
+          <fieldset className={s.subscriptionCosts}>
+            <Typography as={'legend'} variant={'h3'}>
+              {t.profileSettings.tab.accountManagement.yourSubscriptionCosts}
+            </Typography>
+            <div className={s.radioWrapper}>
+              {subscriptionCosts.map((subscription, index) => (
+                <div key={index}>
+                  <input
+                    checked={subscriptionCost === subscription.value}
+                    id={subscription.id}
+                    name={subscription.name}
+                    onChange={subscriptionCostChange}
+                    type={'radio'}
+                    value={subscription.value}
+                  />
+                  <Typography as={'label'} htmlFor={subscription.id} variant={'regular14'}>
+                    {subscription.title}
+                  </Typography>
+                </div>
+              ))}
+            </div>
+          </fieldset>
+          <div className={s.payment}>
+            <Button
+              className={s.paymentBtn}
+              disabled={isLoading}
+              onClick={() => handlePayment('PAYPAL')}
+              variant={'noStyle'}
+            >
+              <PayPal />
+            </Button>
+            <Typography as={'span'} variant={'regular14'}>
+              {t.profileSettings.tab.accountManagement.paymentChoice}
+            </Typography>
+            <Button
+              className={s.paymentBtn}
+              disabled={isLoading}
+              onClick={() => handlePayment('STRIPE')}
+              variant={'noStyle'}
+            >
+              <Stripe />
+            </Button>
+          </div>
+        </div>
+
+        <Modal
+          customButtonsBlock={<></>}
+          modalHandler={() => setIsModalSubscription(false)}
+          open={isModalSubscription}
+          title={
+            isSuccess ? t.modal.successTransactionModalTitle : t.modal.errorTransactionModalTitle
+          }
+        >
+          <div className={s.modalContainer}>
+            <Typography variant={'regular16'}>
+              {isSuccess
+                ? t.modal.successTransactionModalDescription
+                : t.modal.errorTransactionModalDescription}
+            </Typography>
+            <Button fullWidth>{isSuccess ? 'OK' : t.button.backToPayment}</Button>
+          </div>
+        </Modal>
+      </>
+    )
+  }
+)
