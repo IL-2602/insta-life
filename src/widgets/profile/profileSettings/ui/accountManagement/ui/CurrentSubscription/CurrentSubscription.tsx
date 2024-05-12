dcfdace823ecf1483c68238fd5bbf108b47a0454
Mannomann <krivmann00@gmail.com>
2024-05-12 22:15:50 +0200

    IL-116 feat: add traslation for CurrentSubscription

--- src/widgets/profile/profileSettings/ui/accountManagement/ui/CurrentSubscription/CurrentSubscription.tsx
+++ src/widgets/profile/profileSettings/ui/accountManagement/ui/CurrentSubscription/CurrentSubscription.tsx
@@ -1,8 +1,8 @@
 import { useTranslation } from '@/shared/hooks/useTranslation'
+import { Checkbox } from '@/shared/ui/Checkbox'
 import { Typography } from '@/shared/ui/Typography'
 
 import s from '@/widgets/profile/profileSettings/ui/accountManagement/ui/AccountManagement.module.scss'
-import { Checkbox } from '@/shared/ui/Checkbox'
 
 export const CurrentSubscription = () => {
   const { t } = useTranslation()
@@ -14,20 +14,20 @@ export const CurrentSubscription = () => {
       </Typography>
       <div className={s.subscriptionWrapper}>
         <div className={s.subscriptionItem}>
-          <Typography variant={'regular14'} color={'form'}>
-            Expire at
+          <Typography color={'form'} variant={'regular14'}>
+            {t.profileSettings.tab.accountManagement.expireAt}
           </Typography>
           <Typography variant={'bold14'}>12.12.2022</Typography>
         </div>
         <div className={s.subscriptionItem}>
-          <Typography variant={'regular14'} color={'form'}>
-            Next payment
+          <Typography color={'form'} variant={'regular14'}>
+            {t.profileSettings.tab.accountManagement.nextPayment}
           </Typography>
           <Typography variant={'bold14'}>13.02.2023</Typography>
         </div>
       </div>
       <div className={s.checkboxWrapper}>
-        <Checkbox checked={true} label={'Auto-Renewal'} />
+        <Checkbox checked label={t.profileSettings.tab.accountManagement.autoRenewal} />
       </div>
     </fieldset>
   )
