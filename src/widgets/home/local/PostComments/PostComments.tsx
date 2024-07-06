35866ea4dcd79d1d0cb1e59b1f7097a67a85effb
Brainman17 <113635410+Brainman17@users.noreply.github.com>
2024-07-06 15:55:08 +0300

    IL-188 fix: comment opening logic

--- src/widgets/home/local/PostComments/PostComments.tsx
+++ src/widgets/home/local/PostComments/PostComments.tsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useForm } from 'react-hook-form'
 
 import {
   useCreateNewCommentMutation,
   useGetCommentsQuery,
 } from '@/services/commentsAnswersService/commentsAnswersEndpoints'
+import { FillSmallHeart, SmallHeart } from '@/shared/assets/icons/SmallHeart'
+import { TimeDifference } from '@/shared/components/TimeDifference/TimeDifference'
 import { useTranslation } from '@/shared/hooks/useTranslation'
 import { Button } from '@/shared/ui/Button'
 import { Typography } from '@/shared/ui/Typography'
@@ -17,14 +19,25 @@ import { z } from 'zod'
 
 import s from './PostComments.module.scss'
 
-export const PostComments = ({ postId }: Props) => {
+export const PostComments = ({ postId, postIds, time }: Props) => {
   const { t } = useTranslation()
   const { myPostSchema } = usePostSchema()
 
   const [isOpenComments, setIsOpenComments] = useState(false)
+  const [likedComments, setLikedComments] = useState<number[]>([])
 
-  const handleOpenComments = () => {
-    setIsOpenComments(!isOpenComments)
+  const commentLike = (commentId: number) => {
+    if (likedComments.includes(commentId)) {
+      setLikedComments(likedComments.filter(id => id !== commentId))
+    } else {
+      setLikedComments([...likedComments, commentId])
+    }
+  }
+
+  const handleOpenComments = (postId: number) => {
+    if (postIds?.some(id => id === postId)) {
+      setIsOpenComments(!isOpenComments)
+    }
   }
 
   type myPostFormSchema = z.infer<typeof myPostSchema>
@@ -37,17 +50,20 @@ export const PostComments = ({ postId }: Props) => {
     resolver: zodResolver(myPostSchema),
   })
 
-  const [createNewComment, { isLoading: isNewCommentLoading }] = useCreateNewCommentMutation()
-  const { data: comments } = useGetCommentsQuery({ postId })
+  const [createNewComment, { isLoading: isNewCommentLoading, isUninitialized }] =
+    useCreateNewCommentMutation()
+  const { data: comments, isFetching: isGetCommentsLoading } = useGetCommentsQuery({ postId })
 
   const postComment = watch('comment')
 
   const commentPublishHandler = () =>
-    postId &&
-    postComment?.trim() &&
-    createNewComment({ content: postComment, postId })
-      .unwrap()
-      .then(() => setValue('comment', ''))
+    postId && postComment?.trim() && createNewComment({ content: postComment, postId }).unwrap()
+
+  useEffect(() => {
+    if (!isUninitialized) {
+      setValue('comment', '')
+    }
+  }, [isUninitialized, setValue])
 
   return (
     <div className={s.container}>
@@ -55,28 +71,47 @@ export const PostComments = ({ postId }: Props) => {
         as={'button'}
         className={s.viewBtn}
         color={'form'}
-        onClick={handleOpenComments}
+        onClick={() => handleOpenComments(postId)}
         variant={'bold14'}
       >
         {!isOpenComments ? t.post.viewAllcomments : t.post.hideAllComments} (
         <Typography as={'span'}>{comments?.totalCount}</Typography>)
       </Typography>
       <div className={clsx(s.viewComments, isOpenComments ? s.openComments : '')}>
+        {(isNewCommentLoading || isGetCommentsLoading) && isOpenComments && (
+          <div className={s.loader}></div>
+        )}
+
+        {!comments?.items.length && (
+          <Typography className={s.comment} color={'form'} variant={'small'}>
+            {t.post.noComments}
+          </Typography>
+        )}
         {comments?.items.map(comment => {
           return (
             <div className={s.comment} key={comment.id}>
-              <Image alt={'avatar'} height={24} src={comment.from.avatars[0].url} width={24} />
-              <Typography className={s.commentName} variant={'bold14'}>
-                {comment.from.username}
-              </Typography>
-              <Typography variant={'regular14'}>{comment.content}</Typography>
+              <div className={s.commentWrap}>
+                <Image alt={'avatar'} height={24} src={comment.from.avatars[0].url} width={24} />
+                <Typography className={s.commentName} variant={'bold14'}>
+                  {comment.from.username}
+                </Typography>
+                <Typography variant={'regular14'}>{comment.content}</Typography>
+              </div>
+
+              <div className={s.smallHeart} onClick={() => commentLike(comment.id)}>
+                {likedComments.includes(comment.id) ? <FillSmallHeart /> : <SmallHeart />}
+              </div>
             </div>
           )
         })}
       </div>
+      <Typography className={s.time}>
+        <TimeDifference home postTime={time} />
+      </Typography>
       <div className={s.addCommentBlock}>
         <ControlledTextAreaField
           control={control}
+          data-id={`postId-${postId}`}
           name={'comment'}
           placeholder={'Add comment...'}
           textAreaClassName={s.textField}
@@ -96,4 +131,4 @@ export const PostComments = ({ postId }: Props) => {
   )
 }
 
-type Props = { postId: number }
+type Props = { postId: number; postIds: number[] | undefined; time: string }
