e2a95e812d30ed4ed55678cff07a6ea5af6a4fdc
Brainman17 <113635410+Brainman17@users.noreply.github.com>
2024-07-06 15:53:14 +0300

    IL-188 refactor: extract logic from Home component

--- src/widgets/home/publ/ui/Home.tsx
+++ src/widgets/home/publ/ui/Home.tsx
@@ -1,64 +1,14 @@
-import { Fragment, forwardRef } from 'react'
+import { forwardRef } from 'react'
 
-import { PublicPhotos } from '@/shared/components/PubicPhotos/PublicPhotos'
-import { SpinnerThreePoints } from '@/shared/ui/SpinnerThreePoints'
-import { PostComments } from '@/widgets/home/local/PostComments/PostComments'
-import { PostDescription } from '@/widgets/home/local/PostDescription/PostDescription'
-import { PostHeader } from '@/widgets/home/local/PostHeader/PostHeader'
-import { PostIcons } from '@/widgets/home/local/PostIcons/PostIcons'
-import { PostLikes } from '@/widgets/home/local/PostLikes/PostLikes'
+import { HomePosts } from '@/widgets/home/local/HomePosts/HomePosts'
 import { HomeProps } from '@/widgets/home/publ/container'
 
 import s from './Home.module.scss'
 
-export const Home = forwardRef<HTMLDivElement, HomeProps>(({ isFetching, me, posts }, ref) => {
-  if (!posts) {
-    return (
-      <div className={s.fetchSpinner}>
-        <SpinnerThreePoints />
-      </div>
-    )
-  }
-
+export const Home = forwardRef<HTMLDivElement, HomeProps>(({ followers }) => {
   return (
     <div className={s.container}>
-      {posts.items.map(post => {
-        const images = post.images.map(img => img.url)
-
-        return (
-          <Fragment key={post.id}>
-            <PostHeader
-              avatar={post.avatarOwner}
-              time={post.createdAt}
-              userId={me.userId}
-              userName={post.userName}
-            />
-            <PublicPhotos
-              height={505}
-              home
-              id={post.id}
-              ownerId={post.ownerId}
-              photos={images}
-              width={490}
-            />
-            <PostIcons postId={post.id} userId={me.userId} />
-            <PostDescription
-              avatar={post.avatarOwner}
-              description={post.description}
-              userName={post.userName}
-            />
-            <PostLikes postId={post.id} />
-            <PostComments postId={post.id} />
-          </Fragment>
-        )
-      })}
-
-      <div ref={ref}></div>
-      {isFetching && (
-        <div className={s.fetchSpinner}>
-          <SpinnerThreePoints />
-        </div>
-      )}
+      {followers?.items.map((follower, id) => <HomePosts id={follower.userId} key={id} />)}
     </div>
   )
 })
