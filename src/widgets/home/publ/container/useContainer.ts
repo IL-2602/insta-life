5ec6bc1f7c58f456d7c8fd55590bdb50d04b9e78
Brainman17 <113635410+Brainman17@users.noreply.github.com>
2024-07-13 14:39:01 +0300

    IL-188 feat: put home logic in useContainer

--- src/widgets/home/publ/container/useContainer.ts
+++ src/widgets/home/publ/container/useContainer.ts
@@ -1,22 +1,30 @@
-import { useGetMeQuery } from '@/services/authService/authEndpoints'
-import { UserType } from '@/services/authService/lib/authEndpoints.types'
-import {
-  useGetUserFollowersQuery,
-  useGetUserFollowingQuery,
-} from '@/services/usersService/usersEndpoints'
+import { useEffect, useState } from 'react'
+import { useInView } from 'react-intersection-observer'
+
+import { useGetHomePostsQuery } from '@/services/publicService/publicEndpoints'
 
 export const useContainer = () => {
-  const { data: me } = useGetMeQuery() as { data: UserType }
-
-  const { data: followers } = useGetUserFollowersQuery({
-    pageSize: 1,
-    username: me?.userName,
+  const { inView, ref } = useInView({
+    threshold: 1,
   })
 
-  const { data: following } = useGetUserFollowingQuery({
-    pageSize: 4,
-    username: me?.userName,
-  })
+  const [lastPostId, setLastPostId] = useState<number | undefined>(undefined)
 
-  return { following, me }
+  const {
+    data: posts,
+    isFetching,
+    isLoading: isInitialLoading,
+  } = useGetHomePostsQuery({ endCursorPostId: lastPostId, pageSize: !lastPostId ? 4 : 3 })
+
+  useEffect(() => {
+    if (posts && posts.items.length >= posts.totalCount) {
+      return
+    }
+
+    if (inView && posts && posts.items.length > 0) {
+      setLastPostId(posts.items[posts.items.length - 1].id)
+    }
+  }, [inView])
+
+  return { isFetching, isInitialLoading, posts, ref }
 }
--- src/widgets/home/publ/ui/Home.tsx
+++ src/widgets/home/publ/ui/Home.tsx
@@ -1,8 +1,6 @@
-import { Fragment, forwardRef, useEffect, useState } from 'react'
-import { useInView } from 'react-intersection-observer'
+import { Fragment, forwardRef } from 'react'
+import { SkeletonTheme } from 'react-loading-skeleton'
 
-import { useGetPostsQuery } from '@/services/postService/postEndpoints'
-import { useGetUserPostsQuery } from '@/services/publicService/publicEndpoints'
 import { PublicPhotos } from '@/shared/components/PubicPhotos/PublicPhotos'
 import { SpinnerThreePoints } from '@/shared/ui/SpinnerThreePoints'
 import { PostComments } from '@/widgets/home/local/PostComments/PostComments'
@@ -14,93 +12,65 @@ import { HomeProps } from '@/widgets/home/publ/container'
 
 import s from './Home.module.scss'
 
-export const Home = forwardRef<HTMLDivElement, HomeProps>(({ following }) => {
-  const { inView, ref } = useInView({
-    threshold: 1,
-  })
-
-  const [lastPostId, setLastPostId] = useState<number | undefined>(undefined)
-
-  const fillowersIds = following?.items.map(follower => follower.userId)
-  const followersUsername = following?.items.map(follower => follower.userName)
-
-  const {
-    data: posts,
-    isFetching,
-    isLoading,
-  } = useGetUserPostsQuery({
-    endCursorPostId: lastPostId,
-    pageSize: !lastPostId ? 3 : 2,
-    userId: 3,
-  })
-
-  // const { data: userPosts } = useGetPostsQuery({
-  //   username: 'egor5555',
-  // })
-
-  useEffect(() => {
-    if (posts && posts.items.length >= posts.totalCount) {
-      return
-    }
-
-    if (inView && posts && posts.items.length > 0) {
-      setLastPostId(posts.items[posts.items.length - 1].id)
-    }
-  }, [inView])
-
-  if (!followersUsername) {
-    return
-  }
-
-  if (!posts || isLoading) {
-    return (
-      <div className={s.spinner}>
-        <SpinnerThreePoints />
-      </div>
-    )
-  }
-
-  return (
-    <div className={s.container}>
-      {posts?.items.map((post, _, initialArray) => {
-        const images = post.images.map(img => img.url)
-        const postIds = initialArray.map(post => post.id)
-
-        return (
-          <Fragment key={post.id}>
-            <PostHeader
-              avatar={post.avatarOwner}
-              time={post.createdAt}
-              userId={post.ownerId}
-              userName={post.userName}
-            />
-            <PublicPhotos
-              className={s.photos}
-              height={505}
-              home
-              id={post.id}
-              ownerId={post.ownerId}
-              photos={images}
-              width={490}
-            />
-            <PostIcons postId={post.id} userId={post.ownerId} username={post.userName} />
-            <PostDescription
-              avatar={post.avatarOwner}
-              description={post.description}
-              userName={post.userName}
-            />
-            <PostLikes postId={post.id} />
-            <PostComments postId={post.id} postIds={postIds} time={post.createdAt} />
-          </Fragment>
-        )
-      })}
-
-      <div className={s.inView} ref={ref}></div>
-      {isFetching && (
-        <div className={s.fetchSpinner}>
+export const Home = forwardRef<HTMLDivElement, HomeProps>(
+  ({ isFetching, isInitialLoading, posts }, ref) => {
+    if (!posts || isInitialLoading) {
+      return (
+        <div className={s.spinner}>
           <SpinnerThreePoints />
         </div>
-      )}
-    </div>
-  )
-})
+      )
+    }
+
+    return (
+      <SkeletonTheme baseColor={'#202020'} highlightColor={'#444'}>
+        <div className={s.container}>
+          {posts?.items.map((post, _, initialArray) => {
+            const images = post.images.map(img => img.url)
+            const postIds = initialArray.map(post => post.id)
+
+            return (
+              <Fragment key={post.id}>
+                <PostHeader
+                  avatar={post.avatarOwner}
+                  time={post.createdAt}
+                  userId={post.ownerId}
+                  username={post.userName}
+                />
+                <PublicPhotos
+                  className={s.photos}
+                  height={505}
+                  home
+                  id={post.id}
+                  ownerId={post.ownerId}
+                  photos={images}
+                  width={490}
+                />
+                <PostIcons
+                  description={post.description}
+                  isLiked={post.isLiked}
+                  postId={post.id}
+                  userId={post.ownerId}
+                />
+                <PostDescription
+                  avatar={post.avatarOwner}
+                  description={post.description}
+                  userName={post.userName}
+                />
+                <PostLikes likesCount={post.likesCount} postId={post.id} />
+                <PostComments postId={post.id} postIds={postIds} time={post.createdAt} />
+              </Fragment>
+            )
+          })}
+
+          <div className={s.inView} ref={ref}></div>
+          {isFetching && (
+            <div className={s.fetchSpinner}>
+              <SpinnerThreePoints />
+            </div>
+          )}
+        </div>
+      </SkeletonTheme>
+    )
+  }
+)
